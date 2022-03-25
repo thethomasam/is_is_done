9c856ee5c94707188346f05fece06da2df705df4
thethomasam <samkoshythomas007@gmail.com>
2022-03-26 10:04:43 +1030

    Removed Boiler Plate

---
+++ src/App_copy.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App_copy extends Component {
+    render() {
+        return (
+            <div class="App_copy"> 
+                <div class="container m-5 p-2 rounded mx-auto bg-light shadow">
+                <div class ="row">
+                    
+                <div class="col">
+            <div class="p-1 h1 text-primary text-center mx-auto display-inline-block">
+                <i class="fa fa-check bg-primary text-white rounded p-2"></i>
+                <u>My Todo-s</u>
+            </div>
+                        </div>
+                .dan
+            <div/> 
+                    </div> 
+            </div>wdw
+            </div>
+        );
+    }
+}
+
+export default App_copy;
